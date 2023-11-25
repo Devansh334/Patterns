dcfded33cbea0513eb33b463d61eed1c52978b9f
Devansh334 <151917394+Devansh334@users.noreply.github.com>
2023-11-25 15:34:03 +0530

    Add files via upload

---
+++ pattern1.c
@@ -0,0 +1,31 @@
+/*
+
+Draw following pattern using Only 2 Loops
+* * * * *
+* 2 3 4 *
+* 2 3 4 *
+* 2 3 4 *
+* * * * *
+
+*/
+
+#include<stdio.h>
+int i;
+void main()
+{
+	for(i=1;i<=5;i++){
+		for(int j=1;j<=5;j++){
+			
+			if(i==1||i==5||j==1||j==5){
+				printf("* ");
+			}
+			else{
+				printf("%d ",j);
+			}
+			
+		}
+		
+		printf("\n");
+		
+	}
+}
