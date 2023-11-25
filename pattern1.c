bdb2ca88e1011f17ecd6179b5140bf47e6577139
Devansh334 <devanshgoyal334@gmail.com>
2023-11-25 17:12:38 +0530

    changes done

--- pattern1.c
+++ pattern1.c
@@ -9,23 +9,25 @@ Draw following pattern using Only 2 Loops
 
 */
 
-#include<stdio.h>
+#include <stdio.h>
 int i;
 void main()
 {
-	for(i=1;i<=5;i++){
-		for(int j=1;j<=5;j++){
-			
-			if(i==1||i==5||j==1||j==5){
+	for (i = 1; i <= 5; i++)
+	{
+		for (int j = 1; j <= 5; j++)
+		{
+
+			if (i == 1 || i == 5 || j == 1 || j == 5)
+			{
 				printf("* ");
 			}
-			else{
-				printf("%d ",j);
+			else
+			{
+				printf("%d ", j);
 			}
-			
 		}
-		
+
 		printf("\n");
-		
 	}
 }
