d48a43657e197f46d2810a72902485b863a45284
Devansh334 <devanshgoyal334@gmail.com>
2023-12-01 21:48:16 +0530

    new pattern added

---
+++ pattern8.c
@@ -0,0 +1,31 @@
+/*
+
+Draw following pattern using Only 2 Loops
+1 2 3 4 5
+2 2 3 4 5
+3 3 3 4 5
+4 4 4 4 5
+5 5 5 5 5
+
+*/
+
+#include <stdio.h>
+void main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            if (j <= i)
+            {
+                printf("%d ", i);
+            }
+            else
+            {
+                printf("%d ", j);
+            }
+        }
+        printf("\n");
+    }
+}
