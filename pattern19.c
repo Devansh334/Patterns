a294e13f8dab199327e7bd8c6175c26474c3669b
Devansh334 <devanshgoyal334@gmail.com>
2023-12-03 17:25:31 +0530

    new pattern added

---
+++ pattern19.c
@@ -0,0 +1,30 @@
+/*
+
+Draw following pattern using only 2 loops
+6
+1 6
+1 2 6
+1 2 3 6
+1 2 3 4 6
+1 2 3 4 5 6
+*/
+#include <stdio.h>
+void main()
+{
+    int i, j;
+    for (i = 1; i <= 6; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            if (i == j)
+            {
+                printf("6 ");
+            }
+            else
+            {
+                printf("%d ", j);
+            }
+        }
+        printf("\n");
+    }
+}
