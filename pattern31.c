5451e5319ae716b4dcef1c14c4433f5ec1ccea21
Devansh334 <devanshgoyal334@gmail.com>
2023-12-08 11:29:24 +0530

    new pattern added

---
+++ pattern31.c
@@ -0,0 +1,29 @@
+/*
+
+Draw following pattern using only 2 loops
+
+p
+q p
+r q p
+s r q p
+t s r q p
+
+*/
+
+#include <stdio.h>
+
+void main()
+{
+    int i, j;
+    int p = 112;
+
+    for (i = 1; i <= 5; i++)
+    {
+        int temp = p + i - 1;
+        for (j = 1; j <= i; j++)
+        {
+            printf("%c ", temp--);
+        }
+        printf("\n");
+    }
+}
