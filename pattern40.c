f668615913d87cac8b5ce4d622478ecd5e70b23c
Devansh334 <devanshgoyal334@gmail.com>
2023-12-12 23:32:10 +0530

    new pattern added

---
+++ pattern40.c
@@ -0,0 +1,35 @@
+/*
+Draw following using loops
+
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+
+*/
+#include <stdio.h>
+void main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for (i = 4; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
