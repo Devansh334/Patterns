3cdd6ea3ed5ad959a71b28817e77060d2dfbdfbb
Devansh334 <devanshgoyal334@gmail.com>
2023-12-12 23:05:24 +0530

    new pattern added

---
+++ pattern38.c
@@ -0,0 +1,33 @@
+/*
+Draw following using 2 loops
+
+Z Z Z Z Z Z Z
+          Z
+        Z
+      Z
+    Z
+  Z
+Z Z Z Z Z Z Z
+
+*/
+#include <stdio.h>
+void main()
+{
+    int i, j;
+    for (i = 1; i <= 7; i++)
+    {
+        for (j = 1; j <= 7; j++)
+        {
+
+            if (i + j == 8 || i == 1 || i == 7)
+            {
+                printf("Z ");
+            }
+            else
+            {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
